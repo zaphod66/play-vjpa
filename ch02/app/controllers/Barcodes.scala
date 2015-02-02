0fa982272c11a03359d1aa372651e14ff8ad46ba
Norbert Scheller <norbert.scheller@actian.com>
2015-02-02 12:31:38 +0100

    Product list, details page (barcode)

---
+++ ch02/app/controllers/Barcodes.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+object Barcodes extends Controller {
+  val imageResolution = 144
+  
+  def barcode(ean: Long) = Action {
+    import java.lang.IllegalArgumentException
+    
+    val mimeType = "image/png"
+    
+    try {
+      val imageData = ean13BarCode(ean, mimeType)
+      
+      Ok(imageData).as(mimeType)
+    } catch {
+      case e: IllegalArgumentException => BadRequest("Couldn't generate bar code. Error: " + e.getMessage)
+    }
+  }
+  
+  def ean13BarCode(ean: Long, mimeType: String): Array[Byte] = {
+    import java.io.ByteArrayOutputStream
+    import java.awt.image.BufferedImage
+    import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
+    import org.krysalis.barcode4j.impl.upcean.EAN13Bean
+    
+    val output: ByteArrayOutputStream = new ByteArrayOutputStream
+    val canvas: BitmapCanvasProvider  = new BitmapCanvasProvider(
+        output, mimeType, imageResolution, BufferedImage.TYPE_BYTE_BINARY, false, 0)
+    
+    val barcode = new EAN13Bean()
+    barcode.generateBarcode(canvas, String valueOf ean)
+    canvas.finish
+    
+    output.toByteArray
+  }
+}
---
+++ ch02/build.sbt
@@ -0,0 +1,10 @@
+name := """ch02"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.1"
+
+libraryDependencies += "net.sf.barcode4j" % "barcode4j" % "2.0"
+
