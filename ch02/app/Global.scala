ade6f5410b819aea41521af94e2fc9eb8f66d425
Norbert Scheller <norbert.scheller@actian.com>
2015-02-17 21:21:51 +0100

    first emf within play

--- ch02/app/Global.scala
+++ ch02/app/Global.scala
@@ -1,12 +1,18 @@
 import play.api.{GlobalSettings, Application, Logger}
 
+import models._
+
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.logger.info("Application ch02 started.")
+    
+    VJPAClasses.open()
   }
   
   override def onStop(app: Application) {
     Logger.logger.info("Application ch02 stopped.")
+    
+    VJPAClasses.close()
   }
 }
