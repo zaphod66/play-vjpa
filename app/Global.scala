cdd6bc45d04c3f164b81f1cb15d534ca35c0aa01
Norbert Scheller <norbert.scheller@actian.com>
2015-03-02 16:32:18 +0100

    removed comments in Global.scala

--- app/Global.scala
+++ app/Global.scala
@@ -6,9 +6,6 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.logger.info("Application Play for V/JPA started.")
-    
-//  VjpaDAO.open("jpatest1@towel-ubvm|jpatest2@towel-ubvm|jpatest3@towel-ubvm")
-//  VjpaDAO.open("jpaDB1@towel-ubvm")
   }
   
   override def onStop(app: Application) {
